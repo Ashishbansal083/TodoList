0cb8e99b555c4022e01da9c63affc283b156fb7e
Ashish <bansalgashi083@gmail.com>
2022-12-16 23:05:46 +0530

    third time

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 const TodoList=({todos,setTodos})=> {
+    const handleComplete=(todo)=>{
+        setTodos(
+            todos.map((item)=>{
+                if(item.id===todo.id){
+                    return {...item,completed : !item.completed};
+                }
+                return item;
+            }))
+            
+    }
+    
+    const handleDelete=({id})=>{
+        setTodos(todos.filter((todo)=>todo.id!==id))
+    }
+    
     return (
         <div>
             {todos.map((todo)=>(
@@ -12,14 +27,17 @@ const TodoList=({todos,setTodos})=> {
                     onChange={(event)=>event.preventDefault()}
                     />
                     <div>
-                        <button className='button-complete task-button'>
+                        <button className='button-complete task-button' onClick={()=>handleComplete(todo)}>
                             <i className='fa fa-check-circle'/>
+                            Done
                         </button>
                         <button className='button-edit task-button'>
                             <i className='fa fa-edit'/>
+                            Edit
                         </button>
-                        <button className='button-delete task-button'>
+                        <button className='button-delete task-button' onClick={()=>handleDelete(todo)}>
                             <i className='fa fa-trash'/>
+                            Delete
                         </button>
                     </div>
                 </li>
